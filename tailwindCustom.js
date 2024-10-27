36fcab1ff85481f17d7facd8106e2cb8eb7d6e1a
DevKelvinK <devkelvinkesley@gmail.com>
2024-10-26 22:19:35 -0300

    initial commit

---
+++ tailwindCustom.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'redCustom': '#E3646E',
+        'purpleCustom': '#BB72E9',
+        'blueCustom': '#3996DB',
+        'greenCustom': '#82BC4F',
+        'yellowCustom': '#EABD5F',
+        'gray-100Custom': '#0D0E11',
+        'gray-200Custom': '#16181D',
+        'gray-300Custom': '#292C34',
+        'gray-400Custom': '#878EA1',
+        'gray-500Custom': '#C0C4CE',
+        'gray-600Custom': '#E2E4E9',
+      },
+      fontFamily: {
+        'asap': ["Asap", "sans-serif"],
+        'inconsolata': ["Inconsolata", "sans-serif"],
+        'maven': ["Maven Pro", "sans-serif"],
+      },
+      backgroundImage: {
+        'intro': "url('./src/assets/Background_Intro.png')",
+        'contacts': "url('./src/assets/Background_Intro.png')",
+      },
+    }
+  }
+}
